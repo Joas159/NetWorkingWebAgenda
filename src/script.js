af2e536ef0e1d3115cac3c43166c120e2daa0da4
Joas159 <joassousaesilva@gmail.com>
2022-05-09 18:43:53 -0300

    conclusao networking social

--- src/script.js
+++ src/script.js
@@ -3,6 +3,7 @@ const opItem01 = document.querySelector('#opItem01');
 const cadUsuario = document.querySelector('.cadUsuario');
 const cadContato = document.querySelector('.cadContato');
 const cadCompromisso = document.querySelector('.cadCompromisso');
+const cadAtualizacao = document.querySelector('.cadAtualizacao');
 
 //02
 const divItem02 = document.querySelector('.divItem02 a');
@@ -19,17 +20,20 @@ const fechaSessao = document.querySelector('.fechaSessao');
 const btnencerrarCadUsuario = document.querySelector('#encerrarCadUsuario');
 const btnencerrarCadPessoa = document.querySelector('#encerrarCadPessoa');
 const btnencerrarCadCompromisso = document.querySelector('#encerrarCadCompromisso');
+const btnencerrarCadAtualizacao = document.querySelector('#encerrarCadAtualizacao');
 // --------------
 
 const formCadUsuario = document.querySelector('.formCadUsuario');
 const formCadPessoa = document.querySelector('.formCadPessoa');
 const formCadCompromisso = document.querySelector('.formCadCompromisso');
+const formCadAtualizacao = document.querySelector('.formCadAtualizacao');
 
 //=======================================================================================
 opItem01.addEventListener('click', () => {
     cadUsuario.classList.toggle('escondido');
     cadContato.classList.toggle('escondido');
     cadCompromisso.classList.toggle('escondido');
+    cadAtualizacao.classList.toggle('escondido');
 });
 
 divItem02.addEventListener('click', () => {
@@ -50,16 +54,20 @@ cadUsuario.addEventListener('click', () => {
     formCadUsuario.classList.remove('escondido');
     cadContato.classList.add('escondido');
     cadCompromisso.classList.add('escondido');
+    cadAtualizacao.classList.add('escondido');
     divItem02.classList.add('escondido');
     divItem03.classList.add('escondido');
+    opItem01.classList.add('escondido');
 });
 
 btnencerrarCadUsuario.addEventListener('click', () => {
     formCadUsuario.classList.add('escondido');
     cadContato.classList.remove('escondido');
     cadCompromisso.classList.remove('escondido');
+    cadAtualizacao.classList.remove('escondido');
     divItem02.classList.remove('escondido');
     divItem03.classList.remove('escondido');
+    opItem01.classList.remove('escondido');
 });
 
 //Cadastro de Pessoa
@@ -67,17 +75,20 @@ cadContato.addEventListener('click', () => {
     formCadPessoa.classList.remove('escondido');
     cadCompromisso.classList.add('escondido');
     cadUsuario.classList.add('escondido');
+    cadAtualizacao.classList.add('escondido');
     divItem02.classList.add('escondido');
     divItem03.classList.add('escondido');
-
+    opItem01.classList.add('escondido');
 });
 
 btnencerrarCadPessoa.addEventListener('click', () => {
     formCadPessoa.classList.add('escondido');
     cadCompromisso.classList.remove('escondido');
     cadUsuario.classList.remove('escondido');
+    cadAtualizacao.classList.remove('escondido');
     divItem02.classList.remove('escondido');
     divItem03.classList.remove('escondido');
+    opItem01.classList.remove('escondido');
 });
 
 //Cadastro de Compromisso
@@ -85,14 +96,39 @@ cadCompromisso.addEventListener('click', () => {
     formCadCompromisso.classList.remove('escondido');
     cadContato.classList.add('escondido');
     cadUsuario.classList.add('escondido');
+    cadAtualizacao.classList.add('escondido');
     divItem02.classList.add('escondido');
     divItem03.classList.add('escondido');
+    opItem01.classList.add('escondido');
 });
 
 btnencerrarCadCompromisso.addEventListener('click', () => {
     formCadCompromisso.classList.add('escondido');
     cadContato.classList.remove('escondido');
     cadUsuario.classList.remove('escondido');
+    cadAtualizacao.classList.remove('escondido');
     divItem02.classList.remove('escondido');
     divItem03.classList.remove('escondido');
+    opItem01.classList.remove('escondido');
+});
+
+//Cadastro de Atualização
+cadAtualizacao.addEventListener('click', () => {
+    formCadAtualizacao.classList.remove('escondido');
+    cadContato.classList.add('escondido');
+    cadUsuario.classList.add('escondido');
+    cadCompromisso.classList.add('escondido');
+    divItem02.classList.add('escondido');
+    divItem03.classList.add('escondido');
+    opItem01.classList.add('escondido');
+});
+
+btnencerrarCadAtualizacao.addEventListener('click', () => {
+    formCadAtualizacao.classList.add('escondido');
+    cadContato.classList.remove('escondido');
+    cadCompromisso.classList.remove('escondido');
+    cadUsuario.classList.remove('escondido');
+    divItem02.classList.remove('escondido');
+    divItem03.classList.remove('escondido');
+    opItem01.classList.remove('escondido');
 });
